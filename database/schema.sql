4d8a168b40c689dc35fbc948001bead838bccb75
CURVSITO <calaquito08@gmail.com>
2026-03-18 15:48:54 -0600

    Schema update y documentacion

--- database/schema.sql
+++ database/schema.sql
@@ -17,6 +17,7 @@ institucion VARCHAR(150)
 CREATE TABLE pacientes (
 
 id_paciente SERIAL PRIMARY KEY,
+numero_expediente VARCHAR(50) UNIQUE,
 nombre VARCHAR(150) NOT NULL,
 sexo VARCHAR(10) NOT NULL,
 edad INTEGER,
@@ -30,13 +31,13 @@ CREATE TABLE medicos(
 id_usuario INTEGER REFERENCES usuarios(id_usuario),
 id_medico SERIAL PRIMARY KEY,
 nombre VARCHAR(150) NOT NULL,
-cedula VARCHAR(20) NOT NULL
-
+cedula VARCHAR(20) NOT NULL,
+firma_medico TEXT NOT NULL
 );
 
 CREATE TABLE consultas(
 
-id_establecimiento INTEGER REFERENCES establecimientos(id_establecimiento);
+id_establecimiento INTEGER REFERENCES establecimientos(id_establecimiento),
 id_consulta SERIAL PRIMARY KEY,
 id_paciente INTEGER REFERENCES pacientes(id_paciente),
 id_medico INTEGER REFERENCES medicos(id_medico),
@@ -44,11 +45,17 @@ id_medico INTEGER REFERENCES medicos(id_medico),
 motivo_consulta TEXT,
 fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-padecimiento_actual TEXT,
 antecedentes_heredofamiliares TEXT,
+antecedentes_personales TEXT,
+padecimiento_actual TEXT,
+interrogatorio_sistemas TEXT,
+
 temperatura DECIMAL,
 presion_arterial VARCHAR(10),
 peso DECIMAL,
+frecuencia_cardiaca INTEGER,
+frecuencia_respiratoria INTEGER,
+talla DECIMAL,
 
 diagnostico TEXT,
 codigo_cie10 VARCHAR(10) REFERENCES cie10(codigo),
